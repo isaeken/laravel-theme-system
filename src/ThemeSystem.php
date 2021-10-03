b2749029154fefab144bc00eb8e823af87702373
Isa Eken <hello@isaeken.com.tr>
2021-10-03 03:44:39 +0300

    update version

--- src/ThemeSystem.php
+++ src/ThemeSystem.php
@@ -12,7 +12,7 @@ class ThemeSystem
 
     public const PackageName = 'isaeken/laravel-theme-system';
 
-    public const Version = 'v2.2';
+    public const Version = 'v2.3';
 
     public const CommandPrefix = 'themes:';
 
