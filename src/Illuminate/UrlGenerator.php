0c039c0330465f8470ec9c62c9405fa687942713
Isa Eken <hello@isaeken.com.tr>
2021-10-11 14:06:20 +0300

    add ``assetFrom`` override

--- src/Illuminate/UrlGenerator.php
+++ src/Illuminate/UrlGenerator.php
@@ -29,4 +29,23 @@ class UrlGenerator extends \Illuminate\Routing\UrlGenerator
 
         return $this->removeIndex($root) . '/' . trim($theme . '/' . $path, '/');
     }
+
+    /**
+     * Generate the URL to an asset from a custom root domain such as CDN, etc.
+     *
+     * @param  string  $root
+     * @param  string  $path
+     * @param  bool|null  $secure
+     * @return string
+     */
+    public function assetFrom($root, $path, $secure = null)
+    {
+        $root = $this->formatRoot($this->formatScheme($secure), $root);
+        $theme = Str::snake(theme_system()->getCurrentTheme(), '-');
+        if ($theme === 'default') {
+            $theme = '';
+        }
+
+        return $this->removeIndex($root).'/'.trim($theme . '/' . $path, '/');
+    }
 }
